a87281da0907312a4b9cc7808ebfbc327a8366ec
Ignacio <ignaciooromendia@gmail.com>
2023-08-22 20:28:00 -0300

    Ejercicio 1

--- talleres/taller 1/src/ejercicio1.c
+++ talleres/taller 1/src/ejercicio1.c
@@ -10,8 +10,66 @@
 
 int n;
 int numero_maldito;
+int numero_hijo;
+int pids[10];
+
 int generate_random_number(){
 	return (rand() % n);
+}
+
+void signal_handler() {
+    int rng = generate_random_number();
+    if (rng == numero_maldito) {
+        printf("Ultimas palabras ...\n");
+        exit(EXIT_SUCCESS);
+    }
+    return;
+}
+
+void signal_handler_status() {
+    // pid_t pid = wait(NULL); Para obtener el pid del proceso que acaba de cambiar
+    printf("Cambio de estado el hijo %d\n",numero_hijo);
+    pids[numero_hijo] = -2;
+}
+
+void hijo() {
+    signal(SIGTERM, signal_handler);
+    while(1);
+}
+
+void jugar(int k) {
+    signal(SIGCHLD, signal_handler_status);
+
+    // Crea los hijos
+    for(int i = 0; i < n; i++) {
+        pid_t pid = fork();
+        if (pid == -1) exit(EXIT_FAILURE);
+        if (pid == 0) hijo();
+        else pids[i] = pid;
+    }
+
+    printf("Empieza la ruleta\n");
+
+    // Empieza la ruleta
+    for(int i = 0; i < k; i++) {
+        printf("Ronda %d\n",i+1);
+        for(int j = 0; j < n; j++) {
+            if(pids[j] != -2) {
+                numero_hijo = j;
+                kill(pids[j], SIGTERM);
+                sleep(1);
+            }   
+        }
+    }
+
+    // Notifica los ganadores y termina de matar
+    printf("Ganadores:\n");
+    for(int i = 0; i < n; i++) {
+        if (pids[i] != -2) {
+            printf("Hijo #%d - %d\n",pids[i],i+1);
+            kill(pids[i], SIGKILL);
+        }
+    }
 }
 
 int main(int argc, char const *argv[]){
@@ -23,6 +81,7 @@ int main(int argc, char const *argv[]){
 	numero_maldito = atoi(argv[3]);
 
     // Completar
+    jugar(rondas);
 
     exit(0);
 }
