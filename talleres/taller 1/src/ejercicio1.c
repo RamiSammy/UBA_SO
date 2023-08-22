473d0a6f4fe88f6bda75e2ce037d7415382781ad
Ignacio <ignaciooromendia@gmail.com>
2023-08-22 17:17:42 -0300

    Sube taller 1

---
+++ talleres/taller 1/src/ejercicio1.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+
+int n;
+int numero_maldito;
+int generate_random_number(){
+	return (rand() % n);
+}
+
+int main(int argc, char const *argv[]){
+    //Funcion para cargar nueva semilla para el numero aleatorio
+    srand(time(NULL));
+    
+    n = atoi(argv[1]);
+	int rondas = atoi(argv[2]);
+	numero_maldito = atoi(argv[3]);
+
+    // Completar
+
+    exit(0);
+}
